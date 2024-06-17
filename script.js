4c29fe96952eada4b62ed8276ecc9b2b1de2a84f
marelinguzman <126489728+marelinguzman@users.noreply.github.com>
2024-06-17 13:23:13 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function generatePassword() {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+    let password = '';
+    const passwordLength = 12; // Puedes cambiar la longitud de la contraseña aquí
+
+    for (let i = 0; i < passwordLength; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters[randomIndex];
+    }
+
+    document.getElementById('password').value = password;
+}
